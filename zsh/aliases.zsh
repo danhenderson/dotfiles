affe263383983bc1fd4484f53c9ca3a57b31876b
danhenderson <danhenderson.web@gmail.com>
2016-07-25 08:58:17 +1000

    more utils

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,10 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 # g shorthand for git
-alias g=git
+alias g='git'
+
+# file list shorthands
+alias l='ls -la'
+
+#list todos
+alias ltodo='ls ~/DESKTOP'
