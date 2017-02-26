a6b7ca1f749170ec766e49e94f1237ae26559787
danhenderson <danhenderson.web@gmail.com>
2017-02-27 07:52:22 +1000

    Aliases updates and gitignore npm-debug

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,13 +8,18 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
+alias glo='git pull origin'
+alias glom='git pull origin master'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push'
+alias gpo='git push origin'
+alias gpom='git push origin master'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
+alias gf='git fetch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
